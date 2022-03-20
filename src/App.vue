f0d2c98cb83a43e38b5881c80db52602b247bd25
Addison Shang <shangxindi@gmail.com>
2022-03-20 22:54:55 +0800

    Add amap get location functions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,87 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      latResult: null,
+      lngResult: null,
+      address: null,
+    }
+  },
+  methods: {
+    getLocation() {
+      const that = this;
+      /* eslint-disable */
+
+      // convert to formatted address, cuz browser doesn't allow http get location
+      const geocoderWrapper = function () {
+        AMap.plugin('AMap.Geocoder', function () {
+          var geocoder = new AMap.Geocoder({
+            // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode
+            // city: '010'
+          })
+
+          var lnglat = [that.lngResult, that.latResult]
+
+          geocoder.getAddress(lnglat, function (status, result) {
+            if (status === 'complete' && result.info === 'OK') {
+              // result为对应的地理位置详细信息
+              // console.log('converted')
+              that.address = result.regeocode.formattedAddress
+              // console.log(that.address)
+            }
+          })
+        })
+      }
+
+      AMap.plugin('AMap.Geolocation', function () {
+        var geolocation = new AMap.Geolocation({
+          // 是否使用高精度定位，默认：true
+          enableHighAccuracy: true,
+          // 设置定位超时时间，默认：无穷大
+          timeout: 10000,
+          // // 定位按钮的停靠位置的偏移量
+          // offset: [10, 20],
+          // //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+          // zoomToAccuracy: true,
+          // //  定位按钮的排放位置,  RB表示右下
+          // position: 'RB'
+        })
+
+        geolocation.getCurrentPosition(function (status, result) {
+          if (status == 'complete') {
+            onComplete(result)
+          } else {
+            onError(result)
+          }
+        });
+
+        function onComplete(data) {
+          // data是具体的定位信息
+          that.latResult = data.position.lat || null;
+          that.lngResult = data.position.lng || null;
+          geocoderWrapper()
+        }
+
+        function onError() {
+          // 定位出错
+          that.address = '定位失败'
+
+        }
+      })
+
+
+      /* eslint-disable */
+    }
+  },
+  created() {
+    this.getLocation();
+  }
+}
+</script>
 
 <style>
 #app {
@@ -9,5 +90,4 @@
   font-size: 14px;
   background: #f1f1f1;
 }
-
 </style>
