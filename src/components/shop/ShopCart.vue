288dad00c07ab4a923f339ce2038fd4b27233801
Addison Shang <shangxindi@gmail.com>
2022-04-09 22:07:44 +0800

    Add ShopCart component and basic style

---
+++ src/components/shop/ShopCart.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="shop-cart">
+        <div class="cart-wrapper">
+            <span class="material-icons-outlined">shopping_cart</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.shop-cart {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: pink;
+    height: 7vh;
+}
+</style>
